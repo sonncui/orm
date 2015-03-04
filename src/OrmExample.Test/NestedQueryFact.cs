07302beb362395d3b6a6578279f83068ac85f184
[sonncui] <[740548795@qq.com]>
2015-03-04 23:04:25 +0800

    Add nested selection test.

---
+++ src/OrmExample.Test/NestedQueryFact.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Linq;
+using NHibernate.Linq;
+using OrmExample.Resource;
+using Xunit;
+
+namespace OrmExample.Test
+{
+    public class NestedQueryFact : OrmFactBase
+    {
+        [Fact]
+        public void should_select_nested_data()
+        {
+            Order conanOrder = session
+                .Query<Order>()
+                .Where(order => order.User.Name == "Conan")
+                .Fetch(order => order.OrderItems)
+                .Single();
+
+            Assert.Equal(2015, conanOrder.Date.Year);
+            Assert.Equal("Conan", conanOrder.User.Name);
+            Assert.Equal(3, conanOrder.OrderItems.Count);
+            Assert.True(conanOrder.OrderItems.Any(oi => oi.Product.Name == "Glass"));
+        }
+    }
+}
