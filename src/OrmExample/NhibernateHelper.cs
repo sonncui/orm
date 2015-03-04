20a38239031c0c3de75effa9ea3260289a5721d8
[sonncui] <[740548795@qq.com]>
2015-03-04 20:54:27 +0800

    Create configuration.

---
+++ src/OrmExample/NhibernateHelper.cs
@@ -0,0 +1,39 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace OrmExample
+{
+    public class NhibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory sessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    InitializeSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        private static void InitializeSessionFactory()
+        {
+            var connectionString = MsSqlConfiguration.MsSql2008.ConnectionString(
+                @"Data Source=localhost;Initial Catalog=orm_example;Integrated Security=True");
+
+            _sessionFactory = Fluently
+                .Configure()
+                .Database(connectionString)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<NhibernateHelper>())
+                .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return sessionFactory.OpenSession();
+        } 
+    }
+}
