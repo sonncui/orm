a6070d092874c2a09accbaed950304ba5125f448
[sonncui] <[740548795@qq.com]>
2015-03-16 22:44:24 +0800

    test nested update and delete.

--- src/OrmExample.Test/NestedQueryFact.cs
+++ src/OrmExample.Test/NestedQueryFact.cs
@@ -1,10 +1,12 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using NHibernate.Linq;
 using OrmExample.Resource;
 using Xunit;
-
 namespace OrmExample.Test
+
 {
     public class NestedQueryFact : OrmFactBase
     {
@@ -21,6 +23,52 @@ namespace OrmExample.Test
             Assert.Equal("Conan", conanOrder.User.Name);
             Assert.Equal(3, conanOrder.OrderItems.Count);
             Assert.True(conanOrder.OrderItems.Any(oi => oi.Product.Name == "Glass"));
+        }
+
+        [Fact]
+        public void should_update_nested_data()
+        {
+            var order = CreateOrder();
+            Assert.Equal(2015, order.Date.Year);
+            Assert.Equal("lulu", order.OrderItems[0].Product.Name);
+            Assert.Equal("doudou", order.User.Name);
+        }
+
+        private Order CreateOrder()
+        {
+            var user = new User
+            {
+                Name = "doudou"
+            };
+            session.Save(user);
+
+            var order = new Order
+            {
+                Date = DateTime.UtcNow,
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Product = new Product()
+                        {
+                            Name = "lulu"
+                        }
+                    }
+                },
+                User = user
+            };
+            session.Save(order);
+            session.Flush();
+            return order;
+        }
+
+        [Fact]
+        public void should_delete_nested_data()
+        {
+            var order = CreateOrder();
+            session.Delete(order);
+            session.Flush();
+            Assert.Equal(0, session.Query<Order>().Count(o => o.User.Name == "doudou"));
         }
     }
 }
